9bf56f932da1aceffa10d872320f7efc1b478a8a
sangram2093 <sangram2093@gmail.com>
2025-05-31 00:10:51 +0530

    Update graph_builder.py

--- utils/graph_builder.py
+++ utils/graph_builder.py
@@ -12,16 +12,16 @@ def create_graph(data_json):
     data = json.loads(data_json)
 
     with driver.session() as session:
-        for rel in data.get("relationships", []):
-            subj = rel["subject"]
-            verb = rel["verb"]
-            obj = rel["object"]
-
-            subj_id = str(uuid.uuid4())
-            obj_id = str(uuid.uuid4())
-
+        for entity in data.get("entities", []):
             session.run("""
-                MERGE (s:Entity {id: $subj_id, name: $subj})
-                MERGE (o:Entity {id: $obj_id, name: $obj})
+                MERGE (e:Entity {id: $id})
+                SET e.name = $name, e.type = $type
+            """, id=entity["id"], name=entity["name"], type=entity.get("type", "unknown"))
+
+        for rel in data.get("relationships", []):
+            session.run("""
+                MATCH (s:Entity {id: $subject_id})
+                MATCH (o:Entity {id: $object_id})
                 MERGE (s)-[:ACTION {name: $verb}]->(o)
-            """, subj=subj, verb=verb, obj=obj, subj_id=subj_id, obj_id=obj_id)
+            """, subject_id=rel["subject_id"], verb=rel["verb"], object_id=rel["object_id"])
+
